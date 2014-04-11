4c4d67f3026b17fdddf2da4dcb652fd296973679
Calvin Jung <ediyacoo@gmail.com>
2014-04-11 16:33:30 -0700

    getEvent from SD Convention Center

---
+++ ws/getEvent_SDConventionCenter.py
@@ -0,0 +1,84 @@
+import requests, json, time, cgi, dateutil.parser, re
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+from cityBuddy import *
+col=MongoClient().cityBuddy.event
+dateParser=dateutil.parser
+
+print ''
+
+
+#LOG
+LOGGER=getLogger(logName='getEvent_SDConventionCenter.log')
+
+
+
+#send a request to KIMONO to collect events from SD Convention center
+url='http://www.kimonolabs.com/api/7nio609i?apikey=bcd2e8fd4d5d965561a3b3ac6397d0af'
+
+
+#log
+LOGGER.info("-"*60)
+LOGGER.info("START GETTING EVENTS FROM SEATGEEK  AT " + str(datetime.now()))
+LOGGER.info("-"*60)
+
+
+res=requests.get(url)
+j=res.json()
+
+
+#log the meta in the result
+LOGGER.info(json.dumps(j))
+
+#parse each event
+results=[]
+event={}
+category={}
+for evt in j['results']['collection1']:
+
+    try:
+        event=createEvent()
+        category=createCategory()
+        
+        event['title']=evt['title']
+        event['open']= "Private" if evt['openToPublic']=='No' else 'Public'
+        event['attendance']=evt['attendance']
+        event['source']='SD Convention Center'
+
+        #date
+        dates=re.split('\xa0-\xa0',evt['date'])
+        event['datetime_local']=dateParser.parse(dates[0]).isoformat() #convert time to iso format
+        event['endtime_local']=dateParser.parse(dates[1]).isoformat()
+        
+
+        category['name']=evt['type']
+        event['categories'].append(category)
+
+        results.append(event)
+    except Exception, e:
+        #log how many new events
+        LOGGER.info('[ERROR] Parsing events from Seatgeek results: '+ str(e))
+
+        print '[ERROR] Parsing events from Seatgeek results'
+        print json.dumps(str(e))
+
+
+#save in mongodb
+if(len(results)!=0):
+    #log how many new events
+    LOGGER.info('There are ' + str(len(results)) + ' events added in the mongodb')
+    
+    #col.insert(results)
+    print 'Successfully get event from SeatGeek at ' + str(datetime.now())
+
+
+else:
+    #log 
+    LOGGER.info('There are 0 events added in the mongodb')
+    print '[ERROR] No results'
+
+
+
+        
+
+
---
+++ ws/getEvent_seatGeek.py
@@ -0,0 +1,104 @@
+import requests, json, time, cgi
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+from cityBuddy import *
+col=MongoClient().cityBuddy.event
+
+print ''
+
+fields=cgi.FieldStorage
+
+#functions==================================================================
+#get value from URL parameters
+def getURLValue(parameter):
+        return none if parameter not in fields else fields[parameter].value
+
+#===========================================================================
+
+#LOG
+LOGGER=getLogger(logName='getEvent_seatGeek.log')
+
+
+
+#send a request to seatgeek to obtain events at San Diego central point within 100 miles
+url='http://api.seatgeek.com/2/events?per_page=10000&page=1&'
+lat='32.715'
+lon='-117.1625'
+range='40mi'  #or kilometer with 'km'
+url=url+'lat='+lat+'&lon='+lon+'&range='+range
+
+
+#log
+LOGGER.info("-"*60)
+LOGGER.info("START GETTING EVENTS FROM SEATGEEK  AT " + str(datetime.now()))
+LOGGER.info("-"*60)
+
+
+res=requests.get(url)
+j=res.json()
+
+
+#log the meta in the result
+LOGGER.info(json.dumps(j['meta']))
+
+#parse each event
+deletes=['time_tbd', 'links', 'stats', 'date_tbd', 'score', 'datetime_tbd']
+results=[]
+for evt in j['events']:
+
+    try:
+        #if evt.created_at larger than yesterday
+        timeCreatedAt=time.mktime(datetime.strptime(evt['created_at'], '%Y-%m-%dT%H:%M:%S').timetuple())
+        timeYesterday=time.mktime((datetime.utcnow() - timedelta(days=1)).timetuple())
+
+        #timeYesterday=0
+
+        if(timeCreatedAt-timeYesterday>=0):
+            
+            #delete attribute in evt object
+            for d in deletes:
+                del evt[d]
+
+            #change attribute
+            evt['categories']=evt['taxonomies']
+            del evt['taxonomies']
+
+            evt['venue']['latlon']=evt['venue']['location']
+            del evt['venue']['location']
+
+
+            #add attribute
+            evt['source']='seatGeek'
+            evt['reviews']={}
+            evt['access_utc']=str(datetime.utcnow())
+
+            results.append(evt)
+        
+        
+    except Exception, e:
+        #log how many new events
+        LOGGER.info('[ERROR] Parsing events from Seatgeek results: '+ str(e))
+
+        print '[ERROR] Parsing events from Seatgeek results'
+        print json.dumps(str(e))
+
+
+#save in mongodb
+if(len(results)!=0):
+    #log how many new events
+    LOGGER.info('There are ' + str(len(results)) + ' events added in the mongodb')
+    
+    col.insert(results)
+    print 'Successfully get event from SeatGeek at ' + str(datetime.now())
+
+
+else:
+    #log 
+    LOGGER.info('There are 0 events added in the mongodb')
+    print '[ERROR] No results'
+
+
+
+        
+
+
