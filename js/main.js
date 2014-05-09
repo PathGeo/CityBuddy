73d638a6d3e48bf7f743836fc3e9599673818581
Calvin Jung <ediyacoo@gmail.com>
2014-05-09 14:36:48 -0700

    read from json

--- js/main.js
+++ js/main.js
@@ -10,15 +10,6 @@ var app={
 
 $(function(){
 	
-	$('.sidebar li').click(function(){
-		var $this=$(this),
-			href=$this.find('>a').attr('href');
-		
-		$this.siblings().removeClass('active');
-		$this.addClass('active');
-
-		console.log(href);
-	});
 	
 	
 	searchEvent();
@@ -49,44 +40,53 @@ function searchEvent(location){
 
 
 
+
 /**
  * showSearchResult 
  * @param {Object} json
  */
 function showSearchResult(json){
-	var html=''
+	var html='<div class="carousel-inner">',
+		length=json.length,
+		$target=$("#WeekHotEvent"),
+		html_index="<ol class='carousel-indicators'>",
+		venue,
+		star="<img src='images/1399611719_star.png' alt='Social Rank'/>&nbsp; &nbsp; ";
 	
+
+	//read json
 	$.each(json, function(i, evt){
 		evt.thumbnail=evt.thumbnail || "images/1394974803_plan.png";
 		var description=String(evt.description).substr(0, 300) + '.....';
+		venue=evt.venue;
+		reviews=evt.reviews; 
+		
+		//insert index
+		html_index+="<li data-target='#WeekHotEvent' data-slide-to='"+i+"' class='"+((i==0)?"active":"")+"'></li>";
 		
 		//html
-		html='<li>'+
-				 '<img class="result-image" src="' +evt.thumbnail+ '" />'+
-				 '<div class="result-info">'+
-				 	'<div class="result-name">' +((evt.url && evt.url.wesbsite)?"<a href='"+evt.url.website+"' target='_blank'>"+evt.name+"</a>":evt.name)+'</div>'+
-				 	'<div class="result-time">'+evt.time+'</div>'+
-				 	'<div class="result-location">'+evt.location+'</div>'+
-				 	'<div class="result-social">social</div>'+
-				 	'<div class="result-description">'+description+'</div>'+
-				 '</div>'+
-				 '<img class="result-more" src="images/1394835495_information-frame_blue.png" data-id="'+i+'" />'+
-			 '</li>';
+		html+="<div class='item"+((i==0)?" active":"")+"'>"+
+			 	"<img src='"+evt.thumbnail+"'>"+
+			 	"<div class='carousel-caption'>"+
+			 		"<h1>"+evt.name+"</h1>"+
+			 		"<p>"+evt.date+", "+evt.time+" @ <a href='"+ ((venue.url&&venue.url!='')?venue.url:"#")+"' target='_blank'>"+venue.name+"</a><br>"+venue.address+"</p>"+
+			 		"<div class='socialRank'>"+ (function(count){
+			 			//repeat star
+			 			if(count>0){
+			 				return new Array(count+1).join(star);
+			 			}
+			 		})(reviews.rank) +"</div>"+
+			 	"</div>"+
+			 "</div>";
+				
 		
-		$('#searchResult').append(html);
-	})
-	
-	
-	//click event on result-more class
-	$('#searchResult .result-more').click(function(){
-		var $this=$(this);
-		var id=$this.attr('data-id');
-		
-		if(id){
-			showPopup(app.searchResult[id]);
-		}
 	});
+	
+	
+	$target.append(html_index).append(html).carousel();
 }
+
+
 
 
 
